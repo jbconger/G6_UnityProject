cae55800e4c36bb5c871cbfab52585afec9f08ab
Jonathan Conger <jbconger@gmail.com>
2019-09-28 11:33:13 -0600

    Added script folder, cube to default scene, and playerControl script
    Basic movement is still not implemented*

---
+++ G6_TwinStickShooter/Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Player rigidbody
+    public Rigidbody rb;
+
+    // Movement fields
+    public float moveSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Called 50 times per second - if I remember right
+    void FixedUpdate()
+    {
+
+    }
+}
