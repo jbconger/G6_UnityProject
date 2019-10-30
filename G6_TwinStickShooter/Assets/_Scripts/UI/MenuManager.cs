fb8d9bf52ed5535e71b460bc91ba0fe80a45fe2c
Brand <brandon.trimble@outlook.com>
2019-10-30 13:32:40 -0600

    UI MenuManager Script
    _Scripts>UI>MenuManager

---
+++ G6_TwinStickShooter/Assets/_Scripts/UI/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If escape(on keyboard) or start(on controller) are pressed, make the game object active
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7) || Input.GetKeyDown(KeyCode.Joystick2Button7))
+        {
+            pauseMenu.gameObject.SetActive(!pauseMenu.gameObject.activeSelf);
+        }
+
+    }
+}
