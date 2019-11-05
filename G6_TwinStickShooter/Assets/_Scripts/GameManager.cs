d541e14debd454849a57e99d7d25c4b491e2d532
Jonathan Conger <jbconger@gmail.com>
2019-11-05 16:31:38 -0700

    hooked back up menu functions

--- G6_TwinStickShooter/Assets/_Scripts/GameManager.cs
+++ G6_TwinStickShooter/Assets/_Scripts/GameManager.cs
@@ -4,11 +4,22 @@ public class GameManager : MonoBehaviour
 {
 	public static GameManager instance = null;
 
+	public GameObject p1;
+	private Transform p1Spawn;
+	
+	public GameObject p2;
+	private Transform p2Spawn;
+
 	private void Awake()
 	{
 		if (instance == null)
 			instance = this;
 		else if (instance != this)
 			Destroy(gameObject);
+	}
+
+	void NewRound()
+	{
+
 	}
 }
