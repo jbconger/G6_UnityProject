1d669a9d9a5b3f07902a1c3f0b48b8501c116062
Jonathan Conger <jbconger@gmail.com>
2019-11-05 10:12:02 -0700

    Tinkering with a new camera script

---
+++ G6_TwinStickShooter/Assets/_Scripts/Camera/MultiTargetCamera.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MultiTargetCamera : MonoBehaviour
+{
+	public List<Transform> targets;
+	public Vector3 offset;
+	
+	public float smoothTime = 5f;
+
+	public float minZoom = 20f;
+	public float maxZoom = 5f;
+	public float zoomLimiter = 5f;
+
+	private Vector3 velocity;
+	private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+		if (targets.Count == 0)
+			return;
+
+		Move();
+		Zoom();
+    }
+
+	private void Move()
+	{
+		Vector3 centerPoint = GetCenterPoint();
+		Vector3 newPosition = centerPoint + offset;
+		transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+	}
+
+	void Zoom()
+	{
+		float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+		cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+	}
+
+	Vector3 GetCenterPoint()
+	{
+		if (targets.Count == 1)
+		{
+			return targets[0].position;
+		}
+
+		var bounds = new Bounds(targets[0].position, Vector3.zero);
+		for (int i = 0; i < targets.Count; i++)
+		{
+			bounds.Encapsulate(targets[i].position);
+		}
+
+		return bounds.center;
+	}
+
+	float GetGreatestDistance()
+	{
+		var bounds = new Bounds(targets[0].position, Vector3.zero);
+		for (int i = 0; i < targets.Count; i++)
+		{
+			bounds.Encapsulate(targets[i].position);
+		}
+
+		return bounds.size.x;
+	}
+}
---
+++ G6_TwinStickShooter/Assets/_Scripts/RoundLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundLogic : MonoBehaviour
+{
+
+	void Awake()
+	{
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
