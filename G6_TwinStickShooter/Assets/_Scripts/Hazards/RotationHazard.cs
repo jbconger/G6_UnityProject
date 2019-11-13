781d10c73612b2f0ff7959e48e06d1b22760b148
Jonathan Conger <jbconger@gmail.com>
2019-11-13 11:21:02 -0700

    Level fixes, camera fixes, UI prefab updates/fixes, rotating wall script added to lvl 2.

---
+++ G6_TwinStickShooter/Assets/_Scripts/Hazards/RotationHazard.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class RotationHazard : MonoBehaviour
+{
+	public bool active = true;
+	public float rotationSpeed;
+
+	private Vector3 rotate;
+
+	private void Start()
+	{
+		rotate = new Vector3(0f, rotationSpeed, 0f);	
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (active)
+			transform.Rotate(rotate * Time.deltaTime);
+    }
+}
