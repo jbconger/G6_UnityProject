4aeea58c16d1d9fd9269df1de360b061fa0ab6f1
Jonathan Conger <jbconger@gmail.com>
2019-11-21 16:03:47 -0700

    attempt at new control/ui setup

---
+++ G6_TwinStickShooter/Assets/_Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+	// PLAYER INFORMATION
+	public Color m_PlayerColor;
+	public Transform m_SpawnPoint;
+
+	[HideInInspector] public int m_PlayerNumber;
+	[HideInInspector] public string m_ColoredPlayerText;
+	[HideInInspector] public GameObject m_Instance;
+	[HideInInspector] public int m_Wins;
+
+	private ArcherMovement m_Movement;
+	private ArcherShooting m_Shooting;
+	private GameObject m_GameUI;
+
+	public void Setup()
+	{
+		// establish component connections
+		m_Movement = m_Instance.GetComponent<ArcherMovement>();
+		m_Shooting = m_Instance.GetComponent<ArcherShooting>();
+		m_GameUI = m_Instance.GetComponentInChildren<Canvas>().gameObject;
+
+		// set player numbers
+		m_Movement.playerNumber = m_PlayerNumber;
+		m_Shooting.playerNumber = m_PlayerNumber;
+
+		// setup player ui label
+		m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
+
+		// change indicator color
+		m_Instance.GetComponentInChildren<SpriteRenderer>().color = m_PlayerColor;
+	}
+
+	// Used during the phases of the game where the player shouldn't be able to control their tank.
+	public void DisablePlayer()
+	{
+		m_Movement.enabled = false;
+		m_Shooting.enabled = false;
+
+		m_GameUI.SetActive(false);
+	}
+
+	// Used during the phases of the game where the player should be able to control their tank.
+	public void EnablePlayer()
+	{
+		m_Movement.enabled = true;
+		m_Shooting.enabled = true;
+
+		m_GameUI.SetActive(true);
+	}
+
+	// Used at the start of each round to put the tank into it's default state.
+	public void Reset()
+	{
+		m_Instance.transform.position = m_SpawnPoint.position;
+		m_Instance.transform.rotation = m_SpawnPoint.rotation;
+
+		m_Instance.SetActive(false);
+		m_Instance.SetActive(true);
+	}
+}
---
+++ G6_TwinStickShooter/Assets/_Scripts/Player/ArcherShooting.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ArcherShooting : MonoBehaviour
+{
+	// REFERENCES
+	public GameObject arrow;
+	public Transform firePoint;
+	public AudioSource drawArrowSound;
+
+	// PUBLIC FIELDS
+	[HideInInspector] public int playerNumber = 1;
+	public float baseArrowSpeed = 20f;
+	public float chargeDuration = 2f;
+	public float timeBetweenShots = 1f;
+
+	// PRIVATE FIELDS
+	private float chargeTime;
+	private float lastShotTime;
+
+	// ANIMATOR FIELDS
+	private Animator anim;
+
+    // AWAKE, START
+
+    void Start()
+    {
+		anim = GetComponent<Animator>();
+	}
+
+    // UPDATE FUNCTIONS
+
+	// INPUT HANDLERS
+
+	public void Fire(InputAction.CallbackContext ctx)
+	{
+		switch (ctx.phase)
+		{
+			case InputActionPhase.Performed:
+				break;
+
+			case InputActionPhase.Started:
+				chargeTime = Time.time;
+
+				// play sound
+				drawArrowSound.Play();
+
+				// animation
+				anim.SetFloat("Aim", 1);
+				break;
+
+			case InputActionPhase.Canceled:
+				if (Time.time > lastShotTime + timeBetweenShots)
+					Firing();
+				
+				// animation
+				anim.SetFloat("Aim", 0);
+				break;
+		}
+	}
+
+	void Firing()
+	{
+		lastShotTime = Time.time;
+
+		GameObject arw = Instantiate(arrow, firePoint.position, firePoint.rotation);
+		Rigidbody rb = arw.GetComponent<Rigidbody>();
+		arw.GetComponent<Arrow>().ID = this.gameObject.name;
+
+		if (Time.time - chargeTime <= 0.2)
+		{
+			rb.AddForce(firePoint.forward * baseArrowSpeed * 0.5f, ForceMode.Impulse);
+
+		}
+		else if (Time.time - chargeTime <= chargeDuration)
+		{
+			//rb.useGravity = false;
+			rb.AddForce(firePoint.forward * baseArrowSpeed * (Time.time - chargeTime + 0.5f), ForceMode.Impulse);
+		}
+		else
+		{
+			//rb.useGravity = true;
+			rb.AddForce(firePoint.forward * baseArrowSpeed * chargeDuration, ForceMode.Impulse);
+		}
+
+		Destroy(arw, 5f);
+	}
+}
